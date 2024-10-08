31a4064be589ad551ad280d61e691f2b9d09785b
Neto test <svietlana.kabanova@bk.ru>
2024-10-08 12:19:19 +1100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weightKg = 52;
+        double heightM = 1.66;
+        int BMI = service.calculate(weightKg, heightM);
+
+        System.out.println(BMI);
+    }
+}
